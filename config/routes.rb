c8ad773a9299f1eb998035e6e8498cec5381a774
mitchelbade <mitchelm.bade@gmail.com>
2023-01-14 19:49:39 -0800

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show]
+  # resources :birds, only: [:index, :show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "birds#index"
 end
